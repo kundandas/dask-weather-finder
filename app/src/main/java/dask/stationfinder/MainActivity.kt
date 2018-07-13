aeb7e1668c93a3da8f0602e653a6869a66dbd669
Kundan Das <kdas@vmware.com>
2018-07-12 19:57:33 -0700

    Added library skeleton and a method demo call from app to library

--- app/src/main/java/dask/stationfinder/MainActivity.kt
+++ app/src/main/java/dask/stationfinder/MainActivity.kt
@@ -1,12 +1,16 @@
 package dask.stationfinder
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import dask.stationfinderlibrary.Multiplier
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var result: Int = Multiplier().multiply(5, 10)
+        Log.d("MainActivity", "Result of 5*10 from library is $result")
     }
 }
---
+++ stationfinderlibrary/src/main/java/dask/stationfinderlibrary/Multiplier.kt
@@ -0,0 +1,9 @@
+package dask.stationfinderlibrary
+
+class Multiplier{
+
+    fun multiply(a: Int, b:Int): Int  {
+        var result:Int = a*b
+        return result;
+    }
+}
